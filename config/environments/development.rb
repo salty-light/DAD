4bea3d79c0a089f95e454673d2d4d617946c68e5
Raichi Ando <135943914+salty-light@users.noreply.github.com>
2023-11-26 13:44:08 +0000

    device導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "0f177651c5c44354b7b554895a367008.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
